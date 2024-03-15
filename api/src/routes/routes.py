6e00ebfb7670e5c83b3ac9328f91a36409fe3c1f
Kevin Ishimwe <kevinishimwe@DOITs-MacBook-Air.local>
2024-03-15 10:44:54 -0400

    amend

--- api/src/routes/routes.py
+++ api/src/routes/routes.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from api.models import UserModel
 from .serializer import UserSerialize
-from api.middleware.cloud import  handleFileActions
+from api.src.middleware.cloud import  handleFileActions
 
 @api_view(['GET'])
 def root(request):
